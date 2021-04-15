adfcb759a806c8bc8e1f6ecb8b406d22ee654c87
zongronghuang <zongrong.h886@gmail.com>
2021-04-15 17:55:31 +0800

    feat: change Login styles

--- src/views/Login.js
+++ src/views/Login.js
@@ -3,16 +3,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const LoginJSX = ({ className }) => (
   <main className={className}>
-    <div>
-      <header>ASMR Player</header>
-      <span>Immersed in ambient sounds and images</span>
+
+    <div className="circle" id="outer">
+      <div className="circle" id="middle">
+        <div className="circle" id="inner"></div>
+      </div>
     </div>
 
-    <a href="#" id="fb-link">Log in with Facebook</a>
+    <section>
+      <div>
+        <header>ASMR Player</header>
+      </div>
 
-    <footer>
-      <small>Made with love by <a href="#">Zong-Rong</a></small>
-    </footer>
+      <div>
+        <i></i><a href="#" id="fb-link">Log in with Facebook</a>
+      </div>
+
+      <footer>
+        <small>Made with love by <a href="#">Zong-Rong</a></small>
+      </footer>
+    </section>
   </main>
 )
 
@@ -26,40 +36,74 @@ height: 100vh;
 text-align: center;
 background-color: black;
 
+section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 20%;
+  height: 30%;
+  box-shadow: 1px 1px 5px goldenrod, -1px -1px 5px goldenrod, 1px -1px 5px goldenrod, -1px 1px 5px goldenrod;
+  border-radius: 10%;
+}
+
+section > div {
+  width: 100%;
+}
+
 header {
-  margin-bottom: 20px;
-  margin-top: -50px;
   font-family: 'Courgette', cursive, monospace, Arial, sans-serif;
-  font-size: xx-large;
+  font-size: x-large;
   font-weight: bold;
   color: goldenrod;
   text-shadow: 0px 0px 10px goldenrod;
 }
 
-div > span {
-  color: goldenrod;
-  font-family: 'Courgette', cursive, monospace, Arial, sans-serif;
+footer {
+  color: white;
 }
 
-a {
-  text-decoration: none;
+.circle {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  border-top: 1px solid goldenrod;
+  border-bottom: 1px solid goldenrod;
+}
+
+#inner {
+  width: 300px;
+  height: 300px;
+}
+
+#middle {
+  width: 400px;
+  height: 400px;
+}
+
+#outer {
+  width: 500px;
+  height: 500px;
+  transform: translate(-50%, -50%) rotate(45deg);
 }
 
 #fb-link {
   display: block;
-  width: 200px;
+  width: 150px;
   height: 50px;
   background-color: white;
   line-height: 50px;
   border: 1px solid black;
+  text-decoration: none;
+  margin: auto;
 }
 
-footer {
-  border: 1px solid black;
-  width: 100%;
-  margin-bottom: -50px;
-  color: white;
-}
 `
 
 export default Login
