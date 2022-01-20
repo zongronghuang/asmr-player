22a79849147faf99942a156ae938a50f4db96b90
zongronghuang <zongrong.h886@gmail.com>
2022-01-20 14:27:39 +0800

    fix: allow useDragAndDrop to have multiple drag items in one drop zone

--- src/hooks/useDragAndDrop.js
+++ src/hooks/useDragAndDrop.js
@@ -1,23 +1,26 @@
 import { useEffect, useRef } from "react";
 
+// useDragAndDrop 可以在同一個 drop zone 上面，設定一個以上的 drag item
+// drop zone 和 drag item 都要以 ref 的形態傳入
 const useDragAndDrop = ({ dragItemRef, dropZoneRef }) => {
   // 紀錄滑鼠游標和 drag item 原點之間的距離
   // 不用 useState，因為 event listener 只會取得 initial state (closure)，不會更新 => 改用 useRef
   // useState 要和 React 提供的 onClick, onDoubleClick... 併用，才會取得更新的 state
-  const distanceRef = useRef({
+  const dragDistanceRef = useRef({
     left: 0,
     top: 0,
   });
-
+  const isDraggedRef = useRef(false);
   const dragItemEvents = [
     {
       name: "dragstart",
       handler(e) {
         // 取得游標和 drag item 原點的距離
-        distanceRef.current = {
+        dragDistanceRef.current = {
           left: e.clientX - e.target.offsetLeft,
           top: e.clientY - e.target.offsetTop,
         };
+        isDraggedRef.current = true;
       },
     },
     {
@@ -27,7 +30,6 @@ const useDragAndDrop = ({ dragItemRef, dropZoneRef }) => {
       },
     },
   ];
-
   const dropZoneEvents = [
     {
       name: "dragover",
@@ -39,18 +41,26 @@ const useDragAndDrop = ({ dragItemRef, dropZoneRef }) => {
       name: "drop",
       handler(e) {
         // 計算 drag item 原點降落位置 (新的 left 和 top)
+        // 用 isDraggedRef 判斷元件是否被拖曳，被拖曳的元件才會重新計算位置
         e.preventDefault();
-        const { left, top } = distanceRef.current;
+        const { left, top } = dragDistanceRef.current;
+        if (!isDraggedRef.current) {
+          return;
+        }
         const dragItemLeft = e.clientX - left;
         const dragItemTop = e.clientY - top;
         dragItemRef.current.style.left = `${dragItemLeft}px`;
         dragItemRef.current.style.top = `${dragItemTop}px`;
+        isDraggedRef.current = false;
       },
     },
   ];
 
   useEffect(() => {
+    // 讓 drag item 能夠被拖曳
     dragItemRef.current.setAttribute("draggable", true);
+    dragItemRef.current.style.position = "absolute";
+
     dragItemEvents.forEach((e) =>
       dragItemRef.current.addEventListener(e.name, e.handler)
     );
