531c88ae62927da9b7fe6479e955285850cfb8bb
Zong-Rong Huang <zongrong.h886@gmail.com>
2022-01-28 01:33:30 +0800

    refactor: upgrade to firebase v.9 and refactor UseGoogleLogin

--- src/hooks/useGoogleLogin.js
+++ src/hooks/useGoogleLogin.js
@@ -1,39 +1,61 @@
-import { useState } from 'react'
-import firebase from 'firebase/app'
-import 'firebase/auth'
+import { useState } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+// Firebase 初始化
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
 
 const useGoogleLogin = () => {
-  const [GoogleResponse, setGoogleResponse] = useState()
-  const provider = new firebase.auth.GoogleAuthProvider()
+  const [GoogleResponse, setGoogleResponse] = useState();
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
 
   const handleGoogleLogin = async () => {
     try {
-      const result = await firebase.auth().signInWithPopup(provider)
-      const {
-        credential: {
-          accessToken,
-        }
-      } = result
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
 
-      localStorage.setItem('googleAccessToken', accessToken)
-      setGoogleResponse({ login: true })
+      localStorage.setItem("googleAccessToken", token);
+      setGoogleResponse({ login: true });
     } catch (error) {
-      console.log(`[Google] Login failure: code=${error.code} | message=${error.message}`)
-      setGoogleResponse({ login: false })
+      console.log(
+        `[Google] Login failure: code=${error.code} | message=${error.message}`
+      );
+      setGoogleResponse({ login: false });
     }
-  }
+  };
 
   const handleGoogleLogout = async () => {
     try {
-      await firebase.auth().signOut()
-      setGoogleResponse({ login: false })
+      const auth = getAuth();
+      await signOut(auth);
+      setGoogleResponse({ login: false });
     } catch (error) {
-      console.log(`[Google] Logout failure: code=${error.code} | message=${error.message}`)
+      console.log(
+        `[Google] Logout failure: code=${error.code} | message=${error.message}`
+      );
     }
-  }
+  };
 
   // console.log('useGoogleLogin hook ready!')
-  return [GoogleResponse, handleGoogleLogin, handleGoogleLogout]
-}
+  return [GoogleResponse, handleGoogleLogin, handleGoogleLogout];
+};
 
-export default useGoogleLogin
+export default useGoogleLogin;
--- src/index.js
+++ src/index.js
@@ -6,22 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 
 import store from "./redux/store";
 import { Provider } from "react-redux";
-import firebase from "firebase/app";
-
-// Firebase 初始化
-(function () {
-  firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  });
-
-  // console.log('[Init] Google Firebase')
-})();
 
 ReactDOM.render(
   <React.StrictMode>
