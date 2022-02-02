ad037e0c1a2c2031ae86c54c4284c0849acb2932
Zong-Rong Huang <zongrong.h886@gmail.com>
2022-02-02 23:06:48 +0800

    feat: increase total number of tracks to 8

--- src/utils/trackFactory.js
+++ src/utils/trackFactory.js
@@ -3,26 +3,50 @@ import { getRandomImage } from "../apis/unsplash/getRandomImage";
 const orderedTracks = [
   {
     order: 0,
+    name: "Camp fire",
+    searchTerm: "bonfire",
+    audioSrc: "",
+  },
+  {
+    order: 1,
     name: "Bird chirps",
     searchTerm: "forest",
     audioSrc: "",
   },
   {
-    order: 1,
+    order: 2,
+    name: "Flowing stream",
+    searchTerm: "stream",
+    audioSrc: "",
+  },
+  {
+    order: 3,
+    name: "Lively pond",
+    searchTerm: "pond",
+    audioSrc: "",
+  },
+  {
+    order: 4,
     name: "Breaking waves",
     searchTerm: "sea",
     audioSrc: "",
   },
   {
-    order: 2,
+    order: 5,
     name: "Train in motion",
     searchTerm: "train",
     audioSrc: "",
   },
   {
-    order: 3,
+    order: 6,
     name: "Street bustle",
     searchTerm: "street",
+    audioSrc: "",
+  },
+  {
+    order: 7,
+    name: "Ruffling wind",
+    searchTerm: "wind",
     audioSrc: "",
   },
 ];
